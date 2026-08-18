c56128cf12d1e18295e25a32117c3738f197c2cb
Michael Monteiro <michael.devbsb@gmail.com>
2026-08-18 12:44:35 -0300

    Atualização das versões de compileSdk e targetSdk para 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.flowwidget"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.flowwidget"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
